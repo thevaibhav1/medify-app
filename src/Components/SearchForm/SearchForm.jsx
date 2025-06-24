39ff2dfe614eb7aee69594e738bd0f326ac6cbdd
thevaibhav1 <vaibhavsahu593@gmail.com>
2025-06-24 12:08:47 +0530

    medify

--- src/Components/SearchForm/SearchForm.jsx
+++ src/Components/SearchForm/SearchForm.jsx
@@ -16,14 +16,13 @@ const SearchForm = () => {
   };
   return (
     <form className="flex flex-col sm:flex-row gap-4 justify-evenly items-center p-5 mt-5">
-      <div className="relative w-full sm:w-64 text-[16px]">
+      <div className="relative w-full sm:w-64 text-[16px]" id="state">
         <span className="absolute inset-y-0 left-0 flex items-center pl-3">
           <IoIosSearch className="h-5 w-5 text-gray-400" />
         </span>
         <select
           onChange={handleState}
           value={selectstate}
-          id="state"
           className="pl-10 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:font-[poppins]"
         >
           <option value="">State</option>
@@ -35,14 +34,13 @@ const SearchForm = () => {
         </select>
       </div>
 
-      <div className="relative w-full sm:w-64 text-[16px]">
+      <div className="relative w-full sm:w-64 text-[16px]" id="city">
         <span className="absolute inset-y-0 left-0 flex items-center pl-3">
           <IoIosSearch className="h-5 w-5 text-gray-400" />
         </span>
         <select
           onChange={handleCity}
           value={selectcity}
-          id="city"
           className="pl-10 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:font-[poppins]"
         >
           <option value="">City</option>
@@ -59,6 +57,7 @@ const SearchForm = () => {
       <Link to="/Medical">
         <button
           type="submit"
+          id="searchBtn"
           className="bg-blue-500 text-white rounded-md hover:bg-blue-600 transition  font-poppins py-2 w-[15%] text-[16px] min-w-20"
         >
           Submit
